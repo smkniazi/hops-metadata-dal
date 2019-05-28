16562d79ea8e736fb2cd4ace1fb0356c01f945d5
Salman Niazi <smkniazi@kth.se>
2019-05-28 16:33:01 +0200

    Auto generated commit msg.

--- src/main/java/io/hops/transaction/handler/TransactionalRequestHandler.java
+++ src/main/java/io/hops/transaction/handler/TransactionalRequestHandler.java
@@ -153,7 +153,8 @@ public abstract class TransactionalRequestHandler extends RequestHandler {
                   "RetryCount: " + (tryCount-1) + ", " +
                   "TX Stats -- Setup: " + setupTime + "ms, AcquireLocks: " + acquireLockTime + "ms, " +
                   "InMemoryProcessing: " + inMemoryProcessingTime + "ms, " +
-                  "CommitTime: " + commitTime + "ms. " + t, t);
+                  "CommitTime: " + commitTime + "ms. Locks: "+
+                  getINodeLockInfo(locksAcquirer.getLocks())+". " + t, t);
         }
         if (!(t instanceof TransientStorageException) ||  tryCount > RETRY_COUNT) {
           for(Throwable oldExceptions : exceptions) {
